159d9906df4fcdac392b9c204b5d75de7ab1d782
Saqib <hafizsaqi555@gmail.com>
2024-06-28 18:55:12 +0500

    final commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+SEO
+✅Images optimization
+✅Hero image
+✅Email Js
+Backend - routing
+✅Text modify
+✅Other pages modify
+✅Responsiveness issues (navbar)
+✅Social media Links
--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 // Email JS
-// function sendMail(){
-//     const params = {
-//         name: document.myForm.name.value,
-//         name: document.myForm.email.value,
-//         name: document.myForm.message.value,
-//     }
-// }
 
-// const serviceID = "service_zskwg9n";
-// const templateID = "";
 
-// emailjs.send(serviceID, templateID, params)
-// .then(
-//     res => {
-//         document.myForm.reset();
-//     }
-// )
-// .catch(err => console.log(err))
+
+function sendMail(){
+    const params = {
+        name: document.myForm.name.value,
+        email: document.myForm.email.value,
+        message: document.myForm.message.value,
+    }
+}
+
+const serviceID = "service_92xlg96";
+const templateID = "template_vjk00vz";
+
+emailjs.send(serviceID, templateID, params)
+.then(
+    res => {
+        document.myForm.reset();
+    }
+)
+.catch(err => console.log(err))
 
 
 //Navbar
