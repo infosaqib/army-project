b0554aebe0fd7fd2fcd8a82c523c22db369e4a9a
Saqib <hafizsaqi555@gmail.com>
2024-06-29 23:16:39 +0500

    fix-issues

---
+++ js/script.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    //AOS
+    AOS.init({
+        offset: 120,
+        duration: 750,
+    });
+
+    // Email JS
+    console.log(document.myForm)
+    function sendMail() {
+        const params = {
+            name: document.myForm.name.value,
+            email: document.myForm.email.value,
+            message: document.myForm.message.value,
+        }
+
+
+    const serviceID = "service_92xlg96";
+    const templateID = "template_vjk00vz";
+
+    emailjs.send(serviceID, templateID, params)
+        .then(
+            res => {
+                document.myForm.reset();
+            }
+        )
+        .catch(err => console.log(err))
+    }
+
+    //Navbar
+
+    // console.log(overlay, Header)
+})   
+ const overlay = document.getElementById('overlay');
+    const Header = document.getElementById('header');
+    function showMenu() {
+        overlay.classList.toggle('show');
+        Header.classList.toggle('show');
+
+    }
+    function hideMenu() {
+        overlay.classList.remove('show');
+        Header.classList.remove('show');
+
+    }
